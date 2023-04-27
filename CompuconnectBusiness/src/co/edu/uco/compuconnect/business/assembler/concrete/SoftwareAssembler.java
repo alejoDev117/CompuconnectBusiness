ce1f2e39977e40052b0809ac7f17c222b95e29ea
alejoDev117 <alejandrodev117@gmail.com>
2023-04-27 18:46:16 -0500

    Se crea la interfaz Assembler, asi como concret de algunos Objectos

---
+++ CompuconnectBusiness/src/co/edu/uco/compuconnect/business/assembler/concrete/SoftwareAssembler.java
@@ -0,0 +1,45 @@
+package co.edu.uco.compuconnect.business.assembler.concrete;
+
+import co.edu.uco.compuconnect.business.assembler.Assembler;
+import co.edu.uco.compuconnect.business.domain.SoftwareDomain;
+import co.edu.uco.compuconnect.dto.SoftwareDTO;
+import co.edu.uco.compuconnect.entities.SoftwareEntity;
+
+public final class SoftwareAssembler implements Assembler<SoftwareDomain,SoftwareDTO,SoftwareEntity>{
+	
+	private static final Assembler<SoftwareDomain,SoftwareDTO,SoftwareEntity> INSTANCE = new SoftwareAssembler();
+	
+	
+	
+	private SoftwareAssembler() {
+		super();
+	}
+	
+	
+	public static final Assembler<SoftwareDomain,SoftwareDTO,SoftwareEntity> getInstance(){
+		return INSTANCE;
+	}
+
+	@Override
+	public SoftwareDTO toDTOFromDomain(SoftwareDomain domain) {
+		return SoftwareDTO.create().setIdentificador(domain.getIdentidicador()).setNombre(domain.getNombre()).setVersion(domain.getVersion());
+	}
+
+	@Override
+	public SoftwareDomain toDomainFromDto(SoftwareDTO dto) {
+		return new SoftwareDomain(dto.getIdentidicador(),dto.getNombre(),dto.getVersion());
+	}
+
+	@Override
+	public SoftwareEntity toEntityFromDomain(SoftwareDomain domain) {
+		return new SoftwareEntity(domain.getIdentidicador(),domain.getNombre(),domain.getVersion());
+	}
+
+	@Override
+	public SoftwareDomain toDomainFromEntity(SoftwareEntity entity) {
+		
+		return new SoftwareDomain(entity.getIdentidicador(),entity.getNombre(),entity.getVersion());
+	}
+	
+	
+}
