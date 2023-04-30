82bf4a087271e9a8b87eb7b56d825f99ce67f65f
Alejandro Gómez Orjuela <102133299+alejoDev117@users.noreply.github.com>
2023-04-30 11:50:04 -0500

    .
    .

---
+++ src/co/edu/uco/compuconnect/business/assembler/concrete/CentroInformaticaAssembler.java
@@ -0,0 +1,45 @@
+package co.edu.uco.compuconnect.business.assembler.concrete;
+
+import co.edu.uco.compuconnect.business.assembler.Assembler;
+import co.edu.uco.compuconnect.business.domain.CentroInformaticaDomain;
+import co.edu.uco.compuconnect.dto.CentroInformaticaDTO;
+import co.edu.uco.compuconnect.entities.CentroInformaticaEntity;
+
+public final class CentroInformaticaAssembler implements Assembler<CentroInformaticaDomain,CentroInformaticaDTO,CentroInformaticaEntity>{
+
+	private static final Assembler<CentroInformaticaDomain,CentroInformaticaDTO,CentroInformaticaEntity> INSTANCE = new CentroInformaticaAssembler();
+	
+	
+	private CentroInformaticaAssembler() {
+		super();
+	}
+
+	
+	public static final Assembler<CentroInformaticaDomain,CentroInformaticaDTO,CentroInformaticaEntity> getInstance(){
+		return INSTANCE;
+	}
+	
+	
+	
+	@Override
+	public final CentroInformaticaDTO toDTOFromDomain(CentroInformaticaDomain domain) {
+		return CentroInformaticaDTO.create().setIdentificador(domain.getIdentificador()).setNombre(domain.getNombre()).setUbicacion(domain.getUbicacion()).setPoseeVideoBeam(domain.isPoseeVideoBeam());
+		
+	}
+
+	@Override
+	public final CentroInformaticaDomain toDomainFromDto(CentroInformaticaDTO dto) {
+		return new CentroInformaticaDomain(dto.getIdentificador(),dto.getNombre(),dto.getUbicacion(),dto.isPoseeVideoBeam());
+	}
+
+	@Override
+	public final CentroInformaticaEntity toEntityFromDomain(CentroInformaticaDomain domain) {
+		return new CentroInformaticaEntity(domain.getIdentificador(),domain.getNombre(),domain.getUbicacion(),domain.isPoseeVideoBeam());
+	}
+
+	@Override
+	public final  CentroInformaticaDomain toDomainFromEntity(CentroInformaticaEntity entity) {
+		return new CentroInformaticaDomain(entity.getIdentificador(),entity.getNombre(),entity.getUbicacion(),entity.isPoseeVideoBeam());
+	}
+
+}
